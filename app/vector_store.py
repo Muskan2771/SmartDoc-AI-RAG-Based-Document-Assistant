215e1f19b4257b8649702e9f066be05295635b02
Muskan2771 <155989406+Muskan2771@users.noreply.github.com>
2026-06-01 10:09:07 +0530

    Update vector_store.py

--- app/vector_store.py
+++ app/vector_store.py
@@ -1,13 +1,8 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
-# HuggingFaceEmbeddings, This converts text into numerical vectors (embeddings).
-
 from langchain_community.vectorstores import FAISS
-#FAISS stands for: Facebook AI Similarity Search
-#Purpose: Store vectors, Search similar vectors quickly, Enable semantic search
 
 def create_vectorstore(documents):
 
-    #LangChain downloads and loads the model: sentence-transformers/all-MiniLM-L6-v2 ,This model converts text into vectors.
     embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
@@ -16,8 +11,6 @@ def create_vectorstore(documents):
         documents,
         embeddings
     )
-    #This converts the vector database into a retriever object.
-    #What is a Retriever?
 
-    retriever=vectorstore.as_retriever()
+    retriever = vectorstore.as_retriever()
     return retriever
